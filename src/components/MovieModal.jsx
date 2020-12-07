cadffd72b8056ce4e881d76c725bdca2777956c1
hsravo <hsravo1@gmail.com>
2020-12-07 23:39:22 +0100

    modal complete

--- src/components/MovieModal.jsx
+++ src/components/MovieModal.jsx
@@ -24,14 +24,18 @@ const MovieModal = (props) => {
   return (
     <Modal size="lg" id="moviemodal" show={props.show} onHide={handleClose}>
       <Modal.Header id="modal-header" closeButton>
-        <Modal.Title>{content.Title}  ({content.Year})</Modal.Title>
+        <Modal.Title><h2>{content.Title} ({content.Year})</h2></Modal.Title>
       </Modal.Header>
       <Modal.Body id="modal-body1"><img id="modal-poster" src={content.Poster === "N/A" ? Logo : content.Poster} alt="movie-poster"/></Modal.Body>
       <Modal.Body id="modal-body2">
-      <ul>
-        <li>{content.Released}</li>
-        <li>{content.Plot}</li>
-      </ul>
+        <h4>{content.Plot}</h4>
+      </Modal.Body>
+      <Modal.Body id="modal-body3">
+        <h3>IMDb Rating : {content.imdbRating}</h3>
+        <h6>Genre : {content.Genre}</h6>
+        <h6>Actors : {content.Actors}</h6>
+        <h6>Director : {content.Director}</h6>
+        <h6>Production : {content.Production}</h6>
       </Modal.Body>
       <Modal.Footer id="modal-footer">
         <Button id="modal-button" onClick={handleClose}>
